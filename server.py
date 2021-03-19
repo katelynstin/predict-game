f87d24d6e19c4569454de2f155cd13c2e425f536
Kate Stinson <katestinson@Kates-MacBook-Pro.local>
2021-03-19 13:24:02 -0400

    second commit

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ my_model = load('svc_model.pkl')
 app = connexion.App(__name__, specification_dir="./")
 
 # Read the yaml file to configure the endpoints
-app.add_api("master.yaml")
+app.add_api("project.yaml")
 
 # create a URL route in our application for "/"
 @app.route("/")
